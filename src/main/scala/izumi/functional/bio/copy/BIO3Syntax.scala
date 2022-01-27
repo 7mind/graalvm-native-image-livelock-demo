fdbff63c3dbf9de5bac49083360530315c5d4dac
Kai <450507+neko-kai@users.noreply.github.com>
2022-01-27 22:00:30 +0000

    Minimize further: remove overloads

--- src/main/scala/izumi/functional/bio/copy/BIO3Syntax.scala
+++ src/main/scala/izumi/functional/bio/copy/BIO3Syntax.scala
@@ -4,97 +4,68 @@ object BIO3Syntax {
 
   trait BIO3ImplicitPuns extends BIO3ImplicitPuns1 {
     final def BIOTemporal3(self: Object): Unit = ()
-    final def BIOTemporal3(implicit dummy: DummyImplicit): DummyImplicit = dummy
 
     final def BIOFork3(self: Object): Unit = ()
-    final def BIOFork3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns1 extends BIO3ImplicitPuns2 {
     final def BIOAsync3(self: Object): Unit = ()
-    final def BIOAsync3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns2 extends BIO3ImplicitPuns3 {
     final def BIOConcurrent3(self: Object): Unit = ()
-    final def BIOConcurrent3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns3 extends BIO3ImplicitPuns4 {
     final def BIOParallel3(self: Object): Unit = ()
-    final def BIOParallel3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns4 extends BIO3ImplicitPuns5 {
     final def BIO3(self: Object): Unit = ()
-    final def BIO3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns5 extends BIO3ImplicitPuns6 {
     final def BIOPanic3(self: Object): Unit = ()
-    final def BIOPanic3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns6 extends BIO3ImplicitPuns7 {
     final def BIOBracket3(self: Object): Unit = ()
-    final def BIOBracket3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns7 extends BIO3ImplicitPuns8 {
     final def BIOError3(self: Object): Unit = ()
-    final def BIOError3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns8 extends BIO3ImplicitPuns9 {
     final def BIOApplicativeError3(self: Object): Unit = ()
-    final def BIOApplicativeError3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns9 extends BIO3ImplicitPuns10 {
     final def BIOGuarantee3(self: Object): Unit = ()
-    final def BIOGuarantee3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns10 extends BIO3ImplicitPuns11 {
     final def BIOMonad3(self: Object): Unit = ()
-    final def BIOMonad3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns11 extends BIO3ImplicitPuns12 {
     final def BIOApplicative3(self: Object): Unit = ()
-    final def BIOApplicative3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns12 extends BIO3ImplicitPuns13 {
     final def BIOBifunctor3(self: Object): Unit = ()
-    final def BIOBifunctor3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIO3ImplicitPuns13 extends BIOImplicitPuns14 {
     final def BIOFunctor3(self: Object): Unit = ()
-    final def BIOFunctor3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns14 extends BIOImplicitPuns15 {
-    // Note, as long as these auxilary conversions to BIOMonad/Applicative/Functor syntaxes etc.
-    // have the same output type as BIOMonad3/etc conversions above, they will avoid the specificity rule
-    // and _will not_ clash (because the outputs are equal, not <:<).
-    // If you merge them into `BIOLocalSyntax with BIOMonad3`, they _will_ start clashing
-
-    final def BIOLocal(self: Object)(implicit d1: DummyImplicit): Unit = ()
-    final def BIOLocal(implicit dummy: DummyImplicit): DummyImplicit = dummy
+    final def BIOLocal(self: Object): Unit = ()
   }
   trait BIOImplicitPuns15 extends BIOImplicitPuns16 {
     final def BIOMonadAsk(self: Object): Unit = ()
-    final def BIOMonadAsk(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns16 extends BIOImplicitPuns17 {
     final def BIOAsk(self: Object): Unit = ()
-    final def BIOAsk(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns17 extends BIOImplicitPuns18 {
     final def BIOArrowChoice(self: Object): Unit = ()
-    final def BIOArrowChoice(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns18 extends BIOImplicitPuns19 {
     final def BIOArrow(self: Object): Unit = ()
-    final def BIOArrow(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns19 extends BIOImplicitPuns20 {
     final def BIOProfunctor(self: Object): Unit = ()
-    final def BIOProfunctor(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait BIOImplicitPuns20 {
-    @deprecated("Use Error3", "0.11")
     final def BIOMonadError3(self: Object): Unit = ()
-    @deprecated("Use Error3", "0.11")
-    final def BIOMonadError3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
 
 }
--- src/main/scala/izumi/functional/bio/copy/Syntax3.scala
+++ src/main/scala/izumi/functional/bio/copy/Syntax3.scala
@@ -14,86 +14,65 @@ object Syntax3 {
 
   trait ImplicitPuns extends ImplicitPuns1 {
     final def Temporal3(self: Object): Unit = ()
-    final def Temporal3(implicit dummy: DummyImplicit): DummyImplicit = dummy
 
     final def Fork3(self: Object): Unit = ()
-    final def Fork3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns1 extends ImplicitPuns2 {
     final def Async3(self: Object): Unit = ()
-    final def Async3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns2 extends ImplicitPuns3 {
     final def Concurrent3(self: Object): Unit = ()
-    final def Concurrent3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns3 extends ImplicitPuns4 {
     final def Parallel3(self: Object): Unit = ()
-    final def Parallel3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns4 extends ImplicitPuns5 {
     final def IO3(self: Object): Unit = ()
-    final def IO3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns5 extends ImplicitPuns6 {
     final def Panic3(self: Object): Unit = ()
-    final def Panic3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns6 extends ImplicitPuns7 {
     final def Bracket3(self: Object): Unit = ()
-    final def Bracket3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns7 extends ImplicitPuns8 {
     final def Error3(self: Object): Unit = ()
-    final def Error3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns8 extends ImplicitPuns9 {
     final def ApplicativeError3(self: Object): Unit = ()
-    final def ApplicativeError3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns9 extends ImplicitPuns10 {
     final def Guarantee3(self: Object): Unit = ()
-    final def Guarantee3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns10 extends ImplicitPuns11 {
     final def Monad3(self: Object): Unit = ()
-    final def Monad3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns11 extends ImplicitPuns12 {
     final def Applicative3(self: Object): Unit = ()
-    final def Applicative3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns12 extends ImplicitPuns13 {
     final def Bifunctor3(self: Object): Unit = ()
-    final def Bifunctor3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns13 extends ImplicitPuns14 {
     final def Functor3(self: Object): Unit = ()
-    final def Functor3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns14 extends ImplicitPuns15 {
-    final def Local3(self: Object)(implicit d: DummyImplicit): Unit = ()
-    final def Local3(implicit dummy: DummyImplicit): DummyImplicit = dummy
+    final def Local3(self: Object): Unit = ()
   }
   trait ImplicitPuns15 extends ImplicitPuns16 {
     final def MonadAsk3(self: Object): Unit = ()
-    final def MonadAsk3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns16 extends ImplicitPuns17 {
     final def Ask3(self: Object): Unit = ()
-    final def Ask3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns17 extends ImplicitPuns18 {
     final def ArrowChoice3(self: Object): Unit = ()
-    final def ArrowChoice3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns18 extends ImplicitPuns19 {
     final def Arrow3(self: Object): Unit = ()
-    final def Arrow3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
   trait ImplicitPuns19 extends BIO3Syntax.BIO3ImplicitPuns {
     final def Profunctor3(self: Object): Unit = ()
-    final def Profunctor3(implicit dummy: DummyImplicit): DummyImplicit = dummy
   }
 
 }
